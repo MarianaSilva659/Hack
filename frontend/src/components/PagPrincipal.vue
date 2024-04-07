686189ee342252c741492b92e1bf562c92383beb
MarianaSilva569 <marianaantunes659@gmail.com>
2024-04-07 01:47:14 +0100

    falata duas páginas

---
+++ frontend/src/components/PagPrincipal.vue
@@ -0,0 +1,25 @@
+<template>
+    <header class="w3-container" style="position: relative;background-color: #222222;">
+        <div><img src="../assets/logo.jpeg" alt="Imagem de Exemplo" class="w3-image" ref="imag"></div>
+    </header>
+</template>
+
+
+<script>
+export default {
+    data() {
+        return {
+        
+        };
+    },
+
+    methods: {
+        
+    }
+}
+</script>
+
+
+
+<style scoped>
+</style>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,10 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import TheWelcome from '../components/TheWelcome.vue'
 import CreateRoom from '../components/CreateRoom.vue'
+import WaitGroup from '../components/WaitGroup.vue'
+import PagPrincipal from '../components/PagPrincipal.vue'
+
+
 
 const routes = [
   { path: '/', component: TheWelcome },
-  { path: '/create', component: CreateRoom }
+  { path: '/create', component: CreateRoom },
+  { path: '/waitgroup', component: WaitGroup },
+  { path: '/pagPrincipal', component: PagPrincipal }
 ]
 
 const router = createRouter({
